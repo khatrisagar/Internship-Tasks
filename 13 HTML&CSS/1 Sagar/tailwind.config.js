3519ddc79d22968e89347dc603cdf25dd6eddb91
khatrisagar <khatrisagar404@gmail.com>
2023-03-02 18:24:18 +0530

    html and css added

---
+++ 13 HTML&CSS/1 Sagar/tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+    theme:{
+        colors:{
+            transparent:'transparent',
+            navbar: '#5865f2',
+        },
+        extend: {},
+        fontFamily:{
+            'poppins': ['Poppins', 'sans-serif']
+        }
+    }
+}
