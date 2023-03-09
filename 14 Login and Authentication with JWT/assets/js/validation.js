2c82e55b8ba44b67f5740ff0e678eecd1e16c32f
khatrisagar <khatrisagar404@gmail.com>
2023-03-09 12:28:49 +0530

    validation of email and password added to registration

--- 14 Login and Authentication with JWT/assets/js/validation.js
+++ 14 Login and Authentication with JWT/assets/js/validation.js
@@ -1,8 +1,14 @@
-
 var userName;
 var userEmail
 var userPassword
 var userPassword2
+    
+var nameWarning = document.getElementById('name-warning')
+var emailWarning = document.getElementById('email-warning')
+var passwordWarning = document.getElementById('password-warning');
+var passwordWarning2 = document.getElementById('pass2-warning');
+    
+
 
 function checkName(name){
     userName = name
@@ -10,49 +16,56 @@ function checkName(name){
 function checkEmail(email){
     console.log(email)
     userEmail = email
+    fetch(`http://localhost:9999/verify?userEmail=${userEmail}`).then(res=>(res.json())).then(data=>{
+            if(data.match == "yes"){
+                emailWarning.innerHTML = `<p>Your Email already exist</p>`
+                return false;
+            }
+            else if(data.match == "no"){
+                emailWarning.innerHTML = ``
+                return true;
+            }
+        })
 }
 
 function checkPass(pass){
     // console.log(pass)
     userPassword = pass
+    if(userPassword == ""){
+        passwordWarning.innerHTML = ``
+    }
+    if(userPassword != ""){
+        if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/gm.test(userPassword)){
+            passwordWarning.innerHTML = `<p>Your Password is not strong enough</p>`
+        }
+        if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/gm.test(userPassword)){
+            passwordWarning.innerHTML = ``
+        }
+    }
+    
 }
 function checkPass2(pass2){
-    // console.log(pass2)
     userPassword2 = pass2
 
-    if(userPassword == userPassword2){
-        console.log("1",userPassword)
-        console.log("2",userPassword2)
+    if(userPassword2 == ""){
+        passwordWarning2.innerHTML = ``
     }
-    else{
-        console.log("Paass not matched")
-        return false;
+    if(userPassword2 != ""){
+        if(userPassword != userPassword2){
+            passwordWarning2.innerHTML = `<p>Your Confirm Password Not Match</p>`
+            return false
+        }
     }
 }
 
 function checkAll(){
-    fetch(`http://localhost:9999/verify?userEmail=${userEmail}`).then(res=>(res.json())).then(data=>{
-        if(data.match == "yes"){
-            let emailWarning = document.getElementById('email-warning')
-            emailWarning.innerHTML = `<p>Your Email already exist</p>`
-            return false
-        }
-    })
     
-    if(userPassword == userPassword2){
-        console.log("1",userPassword)
-        console.log("2",userPassword2)
-    }
-    else if(userPassword != userPassword2){
-        console.log("Pass not matched")
-        let passwordWarning = document.getElementById('password-warning');
-        let passwordWarning2 = document.getElementById('pass2-warning');
-        
-        passwordWarning2.innerHTML = `<p>Your Confirm Password Not Match</p>`
-
+    if(emailWarning.innerHTML != `` ){
         return false;
     }
-  
-    
-    
+
+    if(userPassword !== userPassword2){
+        return false;
+    }
+       return true;
 }
